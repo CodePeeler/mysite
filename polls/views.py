137711a01d987b99c43adb86cea54a656cdd28c2
codePeeler <sdornan1729@gmail.com>
2013-05-15 20:00:58 +0100

    update views

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def indexx(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	return HttpResponse("<h2>Hello, world.</h2><br /> You're at the poll index.")
+	
+def detail(request, p_id):
+	return HttpResponse("You're looking at poll %s." % p_id)
+	
+def results(request, poll_id):
+	return HttpResponse("You're looking at the resutls of poll %s." % poll_id)
+	
+
+def vote(request, poll_id):
+	return HttpResponse("You're voting on poll %s." % poll_id)
